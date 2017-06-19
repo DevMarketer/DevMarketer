99878545e1de3de6fb7217ed75bbc9e32db918ec
J. Alexander Curtis <jalexandercurtis@gmail.com>
2017-06-19 12:06:09 -0600

    Configured JS and CSS for our app, and set up database config settings

--- config/app.php
+++ config/app.php
@@ -12,7 +12,7 @@ return [
     | any other location as required by the application or its packages.
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'DevMarketer'),
 
     /*
     |--------------------------------------------------------------------------
@@ -64,7 +64,7 @@ return [
     |
     */
 
-    'timezone' => 'UTC',
+    'timezone' => 'America/Denver',
 
     /*
     |--------------------------------------------------------------------------
