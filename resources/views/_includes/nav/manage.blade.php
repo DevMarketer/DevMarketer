ee847406729b9e6a39d54b1663a24ad0cba799d9
J. Alexander Curtis <jalexandercurtis@gmail.com>
2017-07-12 21:20:59 -0600

    Finished Episode 11 Video

---
+++ resources/views/_includes/nav/manage.blade.php
@@ -0,0 +1,18 @@
+<div class="side-menu">
+  <aside class="menu m-t-30 m-l-10">
+    <p class="menu-label">
+      General
+    </p>
+    <ul class="menu-list">
+      <li><a href="{{route('manage.dashboard')}}">Dashboard</a></li>
+    </ul>
+
+    <p class="menu-label">
+      Administration
+    </p>
+    <ul class="menu-list">
+      <li><a href="#">Manage Users</a></li>
+      <li><a href="#">Roles &amp; Permissions</a></li>
+    </ul>
+  </aside>
+</div>
---
+++ resources/views/layouts/manage.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>DevMarketer - MANAGEMENT</title>
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    @yield('styles')
+</head>
+<body>
+
+    @include('_includes.nav.main')
+
+    @include('_includes.nav.manage')
+
+    <div id="app">
+      @yield('content')
+    </div>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}"></script>
+    @yield('scripts')
+</body>
+</html>
