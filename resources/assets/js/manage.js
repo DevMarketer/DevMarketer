c19ab83f1427e0aabe0d90fd6eb9292e66815c14
Ranie Santos <ranietots32@gmail.com>
2017-10-17 22:09:52 +0800

    refactor repetitive code and removed extra semicolons in manage.js

--- resources/assets/js/manage.js
+++ resources/assets/js/manage.js
@@ -1,27 +1,27 @@
 const accordions = document.getElementsByClassName('has-submenu')
 
+function setSubmenuStyles (submenu, maxHeight, margins) {
+  submenu.style.maxHeight = maxHeight
+  submenu.style.marginTop = margins
+  submenu.style.marginBottom = margins
+}
+
 for (var i = 0; i < accordions.length; i++) {
   if (accordions[i].classList.contains('is-active')) {
-    const submenu = accordions[i].nextElementSibling;
-    submenu.style.maxHeight = submenu.scrollHeight + "px";
-    submenu.style.marginTop = "0.75em"
-    submenu.style.marginBottom = "0.75em"
+    const submenu = accordions[i].nextElementSibling
+    setSubmenuStyles(submenu, submenu.scrollHeight + "px", "0.75em")
   }
 
   accordions[i].onclick = function () {
-    this.classList.toggle('is-active');
+    this.classList.toggle('is-active')
 
-    const submenu = this.nextElementSibling;
+    const submenu = this.nextElementSibling
     if (submenu.style.maxHeight) {
       // menu is open, we need to close it now
-      submenu.style.maxHeight = null
-      submenu.style.marginTop = null
-      submenu.style.marginBottom = null
+      setSubmenuStyles(submenu, null, null)
     } else {
       // meny is close, so we need to open it
-      submenu.style.maxHeight = submenu.scrollHeight + "px"
-      submenu.style.marginTop = "0.75em"
-      submenu.style.marginBottom = "0.75em"
+      setSubmenuStyles(submenu, submenu.scrollHeight + "px", "0.75em")
     }
   }
 }
