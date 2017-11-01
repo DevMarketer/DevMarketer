902869b928c14ed51d95d6532d202feda7b2a2a4
NERDYLIZARD <bouasavanhhop@gmail.com>
2017-11-01 15:50:47 +0530

    User.Edit: remove b-radio-group

--- resources/views/manage/users/edit.blade.php
+++ resources/views/manage/users/edit.blade.php
@@ -30,20 +30,18 @@
 
           <div class="field">
             <label for="password" class="label">Password</label>
-            <b-radio-group v-model="password_options">
-              <div class="field">
-                <b-radio name="password_options" value="keep">Do Not Change Password</b-radio>
-              </div>
-              <div class="field">
-                <b-radio name="password_options" value="auto">Auto-Generate New Password</b-radio>
-              </div>
-              <div class="field">
-                <b-radio name="password_options" value="manual">Manually Set New Password</b-radio>
-                <p class="control">
-                  <input type="text" class="input" name="password" id="password" v-if="password_options == 'manual'" placeholder="Manually give a password to this user">
-                </p>
-              </div>
-            </b-radio-group>
+            <div class="field">
+              <b-radio name="password_options" v-model="password_options" native-value="keep">Do Not Change Password</b-radio>
+            </div>
+            <div class="field">
+              <b-radio name="password_options" v-model="password_options" native-value="auto">Auto-Generate New Password</b-radio>
+            </div>
+            <div class="field">
+              <b-radio name="password_options" v-model="password_options" native-value="manual">Manually Set New Password</b-radio>
+              <p class="control">
+                <input type="text" class="input" name="password" id="password" v-if="password_options == 'manual'" placeholder="Manually give a password to this user" required>
+              </p>
+            </div>              
           </div>
         </div> <!-- end of .column -->
 
